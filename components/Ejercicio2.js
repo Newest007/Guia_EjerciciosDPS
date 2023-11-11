b37a373d3dbd5a6e76faab80abec271be3e2e8bd
Arian <adilsonarian@outlook.com>
2023-11-10 19:39:20 -0600

    Avanzando ejercicio 2

--- components/Ejercicio2.js
+++ components/Ejercicio2.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import { View, Button, StyleSheet, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Button, StyleSheet, Text, Alert, TextInput, Image } from 'react-native';
+import { SvgUri, SvgWithCssUri } from 'react-native-svg';
 
 const Ejercicio2 = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Página de Ejercicio 2</Text>
-    </View>
-  );
+    const [direccionIP, setDireccionIP] = useState('');
+    const [resultado, setResultado] = useState(null);
+
+    const handleInputChange = (text) => {
+        setDireccionIP(text);
+    };
+    
+    const consultarAPI = () => {
+        // Reemplaza la URL con la dirección de tu API y ajusta la lógica según sea necesario
+        const apiUrl = `http://ipwho.is/${direccionIP}`;
+    
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => {
+            setResultado(data);
+          })
+          .catch((error) => {
+            console.error('Error al consultar la API:', error);
+          });
+    };
+
+
+    return (
+      <>
+        <View style={styles.container}>
+            <TextInput
+                style={styles.input}
+                placeholder="Ingrese la dirección IP"
+                onChangeText={handleInputChange}
+                value={direccionIP}
+            />
+            <Button title="Consultar API" onPress={consultarAPI} />
+        
+            {resultado && (
+                <View style={styles.resultadoContainer}>
+                <Text>Resultado de la API:</Text>
+                <Text>Tipo de IP:</Text>
+                <Text>{resultado.type}</Text>
+                <Text>Continente:</Text>
+                <Text>{resultado.continent}</Text>
+                <Text>País perteneciente:</Text>
+                <Text>{resultado.country}</Text>
+                <Text>Código del país:</Text>
+                <Text>{resultado.country_code}</Text>
+                <Text>Región:</Text>
+                <Text>{resultado.region}</Text>
+                <Text>Ciudad:</Text>
+                <Text>{resultado.city}</Text>
+                <Text>Capital:</Text>
+                <Text>{resultado.capital}</Text>
+                <Text>{resultado.flag.img}</Text>
+                <Image
+                  uri={resultado.flag.img}
+                  />
+                <SvgUri
+                  width={300}
+                  height={400}
+                  style={styles.svgImage}
+                  uri={resultado.flag.img}
+                  />
+                <Text>Hora Actual:</Text>
+                <Text>{resultado.timezone.current_time}</Text>
+                <Text>Datos de Conexión:</Text>
+                <Text>ISP {resultado.connection.isp}</Text>
+                <Text>ORG {resultado.connection.org}</Text>
+                <Text>Dominio {resultado.connection.domain}</Text>
+                </View>
+                )}
+        </View>
+      </>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -14,6 +81,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  resultadoContainer: {
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+  svgImage: {
+    alignSelf: 'center',
+    justifyContent: 'center',
   },
 });
 
---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJ1O-g062ga8Z_uJLmB3v-haKglh3Nus8",
+  authDomain: "guia-ejercicios.firebaseapp.com",
+  projectId: "guia-ejercicios",
+  storageBucket: "guia-ejercicios.appspot.com",
+  messagingSenderId: "617784933728",
+  appId: "1:617784933728:web:1b201ae0c3b92d60eab205"
+};
+
+// Initialize Firebase
+//const app = initializeApp(firebaseConfig);
